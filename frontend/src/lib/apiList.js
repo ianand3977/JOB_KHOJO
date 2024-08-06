53fadfb9b2e039908b005ee40ae322f4ab527e5c
ianand3977 <anandgu2002@gmail.com>
2024-08-06 16:58:11 +0530

    updated backend routing

--- frontend/src/lib/apiList.js
+++ frontend/src/lib/apiList.js
@@ -1,5 +1,5 @@
-export const server = "http://localhost:4444";
-//export const server = "https://secret-shelf-87665.herokuapp.com";
+//export const server = "http://localhost:4444";
+export const server = "https://job-khojo.onrender.com";
 
 const apiList = {
   login: `${server}/auth/login`,
